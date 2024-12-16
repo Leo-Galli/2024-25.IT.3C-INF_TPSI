31fad7059b2e44987fddaed817ff4dd4bf8bdc7b
EnderCloud-it <postmaster@leodev.it>
2024-12-16 07:47:07 +0000

    commit 1

--- hello.c
+++ hello.c
@@ -5,62 +5,62 @@ int NOT(int A) {
     // Il valore di ritorno della funzione
     return 1 - A;
 }
-
 // Funzione per simulare la porta AND
 int AND(int A, int B) {
     // Il valore di ritorno della funzione
     // TODO aggiungere l'espressione matematica per soddisfare la porta AND
     return A*B;
 }
-
 // Funzione per simulare la porta OR
 int OR(int A, int B) {
     // Il valore di ritorno della funzione
     // TODO aggiungere l'espressione matematica per soddisfare la porta OR
     return (1-(1-A)*(1-B));
 }
-
 int main() {
     // Dichiara una variabile
-    int A, B;
-
+    int A, B, C, D, E, F;
     // Mostra un messaggio sul terminale che l'utente può leggere
     printf("Inserisci il valore di A (0 o 1):\n");
     // Attende che l'utente inserisca un valore e prema invio
     scanf("%d", &A);
     // Mostra un messaggio sul terminale che l'utente può leggere
     printf("Hai inserito il valore A: %d\n", A);
-
     // Mostra un messaggio sul terminale che l'utente può leggere
     printf("Inserisci il valore di B (0 o 1):\n");
     // Attende che l'utente inserisca un valore e prema invio
     scanf("%d", &B);
     // Mostra un messaggio sul terminale che l'utente può leggere
     printf("Hai inserito il valore B: %d\n", B);
-
     if (A != 0 && A != 1 && B != 0 && B != 1) {
         printf("I valori inseriti non sono 1 o 0\n");
     } else {
         // Procediamo con l'esecuzione solo se il numero è 1 o 0
         printf("I valori inseriti sono 1 o 0\n");
-
         // Esempio di porta NOT
         printf("Il valore di A viene trasformato da una porta NOT\n");
         int notA = NOT(A);
         printf("Il valore di uscita della porta NOT è: %d\n", notA);
-
         // Esempio di porta AND
         printf("I valori di A e B vengono trasformati da una porta AND\n");
         int andAB = AND(A, B);
         printf("Il valore di uscita della porta AND è: %d\n", andAB);
-
         // Esempio di porta OR
         printf("I valori di A e B vengono trasformati da una porta OR\n");
         int orAB = OR(A, B);
         printf("Il valore di uscita della porta OR è: %d\n", orAB);
         int Bonus = NOT(AND(A, OR(A, B)));
         printf("Il valore di uscita della funzione NOT(AND(A, OR(A, B))) è: %d\n", Bonus);
-    }
 
+        int Tautologia = ((((NOT(A), OR(A)), OR(NOT(D), OR(D))), AND((NOT(E), OR(E)), OR (NOT(F), OR(F)))), AND((NOT(B), OR(B)), OR(NOT(C), OR(C))))  
+
+    }
     printf("\n\n");
 }
+
+
+
+
+/* La porta logica teutologia in utilizzo è:  
+((((NOT(A) OR A) OR (NOT(D) OR D)) AND ((NOT(E) OR E) OR (NOT(F) OR F))) AND ((NOT(B) OR B) OR (NOT(C) OR C)))       
+*/
